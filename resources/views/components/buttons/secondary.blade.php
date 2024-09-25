d17d81f4b22cdd89f325dbe271b0e8d75f27d93b
Marcel Stadelmann <m@marceli.to>
2024-09-25 09:37:54 +0200

    wip

---
+++ resources/views/components/buttons/secondary.blade.php
@@ -0,0 +1,4 @@
+@props(['href'])
+<a href="{{ $href }}" {{ $attributes->merge(['class' => 'bg-fern inline-flex items-center hover:bg-olive transition-colors text-white whitespace-nowrap px-5 xl:px-10 py-4 xl:py-8 xl:ml-10 leading-none rounded-full']) }}">
+  {{ $slot }}
+</a>
--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -21,6 +21,60 @@
       src="{{ asset('media/logo-claim.png') }}" 
       alt="Spross Transport & Recycling AG" 
       class="block mx-auto w-full h-auto max-w-4xl" />
+      <div class="xl:mt-100">
+        <div class="flex justify-center items-center gap-10 xl:gap-20">
+          <a 
+            href="https://www.facebook.com/Spross.Gruppe" 
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Spross auf Facebook"
+            class="group">
+            <x-icons.facebook class="text-fern group-hover:text-moss transition-colors" />
+          </a>
+          <a 
+            href="https://www.linkedin.com/company/spross" 
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Spross auf LinkedIn"
+            class="group">
+            <x-icons.linkedin class="text-fern group-hover:text-moss transition-colors" />
+          </a>
+        </div>
+        <div class="text-sm text-center text-fern mt-20 xl:mt-40">
+          &copy; Spross-Gruppe, {{ date('Y') }} &mdash;
+          
+          <a 
+            href="https://www.spross.com/meta/datenschutz"
+            target="_blank"
+            title="Datenschutz"
+            class="inline-block mx-3 hover:text-moss transition-colors hover:underline decoration-1 underline-offset-4">
+            Datenschutz
+          </a>
+          <a
+            href="https://www.spross.com/meta/disclaimer"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Disclaimer"
+            class="inline-block mx-3 hover:text-moss transition-colors hover:underline decoration-1 underline-offset-4">
+            Disclaimer
+          </a>
+          <a 
+            href="https://www.spross.com/meta/impressum"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Impressum"
+            class="inline-block mx-3 hover:text-moss transition-colors hover:underline decoration-1 underline-offset-4">
+            Impressum
+          </a>
+          <x-buttons.secondary 
+            href="/"
+            class="flex gap-x-5">
+            spross.com
+            <x-icons.arrow />
+          </x-buttons.secondary>
+        </div>
+      </div>
+    </div>
   </x-layout.inner>
 </footer>
 @vite('resources/js/app.js')
