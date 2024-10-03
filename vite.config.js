175fc4fe8b2fdf6bc8a0ab6c20dec7640e6a33b2
Marcel Stadelmann <m@marceli.to>
2024-10-03 15:59:40 +0200

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import { resolve } from 'path';
 export default defineConfig({
+  resolve: {
+    alias: {
+      img: resolve('resources/media'),
+      fonts: resolve('resources/fonts')
+    }
+  },
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
